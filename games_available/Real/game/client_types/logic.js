8bde80b855ea04666a9a7e8bde1b0f5497ece7c2
Andrew Bateman <andrew@accredible.com>
2021-04-25 20:21:24 +0100

    Added more text

--- games_available/Real/game/client_types/logic.js
+++ games_available/Real/game/client_types/logic.js
@@ -110,46 +110,83 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
             </table>`,
         },
         {
-          id: 2,
+          id: 'E',
           icon: '',
-          topic: 'Text 2',
+          topic: 'ENSO weather cycle',
           html:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            `<p>Eastern and southern Africa, Latin America and the Asia-Pacific region experience a cyclical extreme weather pattern called ENSO. A warming of the central to eastern tropical Pacific Ocean, ENSO affects millions of people directly. This cycle can have a period of between 3 to 7 years.</p>
+            <p>The three phases of the ENSO cycle are named El Niño, La Nina and Neutral.</p>`
         },
         {
-          id: 3,
+          id: '1A',
           icon: '',
-          topic: 'Text 3',
+          topic: 'El Niño & La El Nina',
           html:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            `<p>El Niño is a weather phenomenon that can have a number of effects, typically it entails excessive rains and floods in South America and severe drought and hotter average temperatures, for most regions of Asia-Pacific. This can frequently lead to food insecurity for affected countries.</p>
+            <p>La Nina is a somewhat opposite weather phenomenon that entails wetter environments and cooler average temperatures, for the same regions of Asia-Pacific.</p>`
         },
         {
-          id: 4,
+          id: '2A',
           icon: '',
-          topic: 'Text 4',
+          topic: 'Weather issues for sugar',
           html:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            `<p>Sugar is regarded as a robust crop, it can be grown in many regions across the world in either sugar cane or sugar beet form. Sugar cane grows best in a consistently temperate climate and with plenty of water. This means the most damaging conditions for sugar are draught, far more so than excess water.</p>`
         },
         {
-          id: 5,
+          id: '3A',
           icon: '',
-          topic: 'Text 5',
+          topic: 'Weather Forecast',
           html:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            `<p>The El Niño weather pattern (as referenced in F) is predicted to begin this March and will carry on throughout the rest of the year.</p>`
         },
         {
-          id: 6,
+          id: '1B',
           icon: '',
-          topic: 'Text 6',
+          topic: 'Price of the dollar',
           html:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            `<p>The depreciating price of the US dollar has caused the price of commodities to increase when imported from foreign countries. This leads farmers to seek internal forms of sale if available.</p>`
+        },
+        {
+          id: '2B',
+          icon: '',
+          topic: 'Brazil\'s move to using sugar as fuel',
+          html:
+            '<p>Brazil is in the midst of a national transition towards favouring transport which uses ethanol instead of fossil fuels to power its engines. Ethanol is a far cheaper alternative to fossil fuels for the nation since ethanol is made from sugar and so Brazilian farmers are able to sell sugar straight to the refineries.</p>',
+        },
+        {
+          id: '3B',
+          icon: '',
+          topic: 'Principal sugar exporting coutries in 2019/2020',
+          html:
+            `<img src="imgs/sugar_exporting_countries.png" width="100%">`,
+        },
+        {
+          id: '1C',
+          icon: '',
+          topic: 'British farming subsidies',
+          html:
+            '<p>Britain relies on exports of most commodities. Typically, those producers inside the UK are given subsidies by the government to keep pricing competitive. Those crops commonly produced in the UK are less subject to global price shocks since they are less reliant on imports.</p>',
+        },
+        {
+          id: '2C',
+          icon: '',
+          topic: 'UK wheat consumption',
+          html:
+            '<p>The total production of wheat in the UK is 80% of that this it consumes.</p>',
+        },
+        {
+          id: '3C',
+          icon: '',
+          topic: 'Global wheat supply',
+          html:
+            '<p>The global wheat supply is expected to shrink this year due to poor weather and other grains taking priority to farmers over the previous year.</p>',
         },
       ];
 
       // Loop through all connected players.
       node.game.pl.each(function (player) {
         // Calculate the infoBar tabs for each player and send them   
-        node.say('INFODATA', player.id, tabData);
+        node.say('INFODATA', player.id, tabData.slice(4, 10));
       });
     },
   });
