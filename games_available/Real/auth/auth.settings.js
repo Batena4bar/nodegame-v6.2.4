7f2dfab6439f594382ebc30ae03897e326576e87
Batena4bar <73076446+Batena4bar@users.noreply.github.com>
2021-08-31 15:06:15 +0100

    push before version update

--- games_available/Real/auth/auth.settings.js
+++ games_available/Real/auth/auth.settings.js
@@ -85,7 +85,7 @@ module.exports = {
      * Modes: 'local'
      * Default: 'codes.json', 'codes.js', 'code.csv' (tried in sequence)
      */
-    inFile: 'prolific.codes.csv',
+    inFile: 'codes.csv',
 
     /**
      * ## dumpCodes
